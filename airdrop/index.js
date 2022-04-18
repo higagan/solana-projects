3713512beca8a2f80cc95cfa928589d92d92d400
higagan <75811438+higagan@users.noreply.github.com>
2022-04-18 18:10:23 +0530

    airdrop to create fake sol

---
+++ airdrop/index.js
@@ -0,0 +1,46 @@
+const { 
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+const secretKey = wallet._keypair.secretKey
+
+const getWalletBalance = async() => {
+    try{
+
+        const connection = new Connection(clusterApiUrl('devnet'),'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`wallet balance is ${walletBalance}`)
+    } catch(err){
+
+        console.error(err)
+    }
+}
+
+const airDropSol = async() => {
+    try{
+        const connection = new Connection(clusterApiUrl('devnet'),'confirmed');
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2* LAMPORTS_PER_SOL)
+        await connection.confirmTransaction(fromAirDropSignature)
+    } catch(err){
+        console.error(err)
+    }
+}
+
+const main = async() => {
+   
+    await airDropSol()
+    await getWalletBalance()
+}
+
+main()
+
+
+
+
